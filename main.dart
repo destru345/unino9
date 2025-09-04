86fd4b5d95b2cb7e96f89ecc09d068292f0de8a1
destru345 <163693779+destru345@users.noreply.github.com>
2025-09-04 00:07:27 +0000

    feat: if statement

---
+++ main.dart
@@ -0,0 +1,21 @@
+//import 'dart:io';
+//void main (){
+  //  int idade = 9;
+  //  if(idade > 18)
+   // {
+   //     print('pode entra na balada');
+    
+    }
+   // print('fin do programa');
+}
+
+import 'dart:io';
+void main (){
+    int idade = 9;
+    if(idade > 18)
+    {
+        print('pode entra na balada');
+    
+    }
+    print('fin do programa');
+}
